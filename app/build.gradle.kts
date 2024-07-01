25a7ff4797853d9b51e496b8b0d3033a770ee9e5
barry.peralta@plantandfood.co.nz <barryperalta@plantandfood.co.nz>
2024-07-01 17:22:00 +1200

    ksp is incompatible with dagger hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.dagger.hilt)
-    alias(libs.plugins.kotlin.ksp)
+    //alias(libs.plugins.kotlin.ksp)
+    id("kotlin-kapt")
 }
 
 android {
@@ -62,11 +63,11 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.room.runtime)
-    ksp(libs.androidx.room.compiler)
+    kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
+    kapt(libs.hilt.android.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
